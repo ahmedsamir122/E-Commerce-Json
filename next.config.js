ca5934c66570fd4ee826e09c95782354976c1989
ahmedsamir122 <eng.ahmed.elmitwalli@gmail.com>
2022-11-19 20:03:35 +0200

    fix img error

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['dummyjson.com','localhost'],
+    domains: ['dummyjson.com','localhost','i.dummyjson.com'],
   },
 }
 
