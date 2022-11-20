90c8dfdb0a383b990cd591e6d8900e9538c25ded
ahmedsamir122 <eng.ahmed.elmitwalli@gmail.com>
2022-11-20 15:41:05 +0200

    aff config

--- Firebase.js
+++ Firebase.js
@@ -6,13 +6,13 @@ import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBwhbp1klk9Pu9zTCJZ_qV6dPjj2NFU9YI",
-  authDomain: "react-test-aefc2.firebaseapp.com",
-  databaseURL: "https://react-test-aefc2-default-rtdb.firebaseio.com",
-  projectId: "react-test-aefc2",
-  storageBucket: "react-test-aefc2.appspot.com",
-  messagingSenderId: "218274088793",
-  appId: "1:218274088793:web:8f1fb8d40954c5c533ee48"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
 };
 
 // Initialize Firebase
